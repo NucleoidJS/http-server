76c491fb8feb77467fb5b4dd7fee4cc04cc3db73
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-02-16 22:44:37 +0300

    Add morgan middleware for logging requests

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const path = require("path");
 const { pathToFileURL, fileURLToPath } = require("url");
 const configPath = pathToFileURL(process.cwd());
+
 const app = express();
+app.use(morgan("tiny"));
 
 import(`${configPath}/config.mjs`).then((module) => {
   const config = module.default;
